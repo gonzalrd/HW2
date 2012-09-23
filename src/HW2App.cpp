fc009245d2c89bb3fabcfcc1d0ca457ceabde25e
Raquel Gonzalez <gonzalrd@muohio.edu>
2012-09-22 22:16:40 -0400

    edits to add and setup

--- src/HW2App.cpp
+++ src/HW2App.cpp
@@ -46,17 +46,22 @@ void HW2App::setup()
 {
 	//creates a rectangle
 	Color8u c = Color8u(0, 60, 120);
+	Color8u c2 = Color8u(0, 120, 60);
+	Color8u c3 = Color8u(250,210,210);
  	oneRect_ = new rectangle(c,200,300,400,500);
 
-	rectangle* twoRect_ = new rectangle(c,100,350,350,450);
+	rectangle* twoRect_ = new rectangle(c2,100,350,500,450); // need to check size, adds behinds so dont see it when it shows up. 
+	rectangle* threeRect_ = new rectangle(c3,60,50,500,500);
 
 	//create a nodes
 	Node* one = new Node();
 	Node* two = new Node();
+	Node* three = new Node();
 
 	//adds rectangles two nodes
 	one->data_= oneRect_;
 	two->data_= twoRect_;
+	three->data_= threeRect_;
 
 	//creates a list
 	lst =  new List();
@@ -65,6 +70,8 @@ void HW2App::setup()
 
 	lst->add(one);
 	lst->add(two);
+	lst->add(three);
+
 }
 
 void HW2App::mouseDown( MouseEvent event )
